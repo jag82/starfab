15151833101aad5973ccf3f021f2c73fbd3a14c7
Jag C <jag.chadha@gmail.com>
2018-05-07 16:14:55 +0200

    updated to work with actual fabric cutting workflow (for model)

--- index.js
+++ index.js
@@ -33,47 +33,52 @@ const fabricHeight = data.height * fudge;
 //widest mark (for a pair of leaves in AV position)
 const middleWidth = marks[5];
 const baseWidth = marks[0];
+const margin = .055;// .055m = 5.5cm 
+const offset = ((baseWidth/2) + (middleWidth) - (baseWidth/2)) + margin;
+print('margin', margin);
+print('offset', offset);
+print('MIDDLE-TO-MIDDLE', offset - (baseWidth/2) );
+printBreak();
 
-// //first leaf keypoints
+// //first side keypoints
 print('base1-L', 0);
 print('base1-C', baseWidth/2);
-print('peak1', baseWidth/2);
 print('base1-R', baseWidth);
+print('peak2', offset + baseWidth/2);
 
 
 printBreak();
 
-// //second leaf keypoints
-const margin = fabricHeight / 10; //space between leaves on fabric (to reduce precise cutting requriements)
+// //second side keypoints
 
-print('margin', margin);
+
+
 printBreak();
 
-const offset = ((baseWidth/2) + (middleWidth) - (baseWidth/2)) + margin;
-
+print('peak1', baseWidth/2);
+console.log('DRAW CENTER LINE');
+print('fabric height', fabricHeight);
 print('base2-L', offset);
 print('base2-C', offset + baseWidth/2);
-print('peak2', offset + baseWidth/2);
 print('base2-R', offset + baseWidth);
 
 printBreak();
 
 
 // fabric height measurements
-
-print('fabric height', fabricHeight);
-
-const unitHeight = data.height / marks.length;
+const unitHeight = fabricHeight / (marks.length-1);
 for (let i = 0; i < marks.length; i++) {
     print(`h${i}`, i*unitHeight);
 }
-
+print('fabric height', fabricHeight);
 printBreak();
 
 
-print('fabric width', baseWidth);
+// print('fabric width', baseWidth);
 for (let i = 0; i < marks.length; i++) {
+    print(`w${i}/2`, marks[i]/2);
     print(`w${i}`, marks[i]);
+    printBreak();
 }
 
 
